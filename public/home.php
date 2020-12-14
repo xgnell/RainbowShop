ff23fc77ebd9347a29babbfcfcf26e401584505c
Dathuha <64651929+Dathuha@users.noreply.github.com>
2020-12-14 09:06:14 +0700

    Save all to fix after

--- public/home.php
+++ public/home.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Giới thiệu</title>
     <link rel="stylesheet" href="/public/templates/css/all.css">
     <style>
-        .disp-items > div {
+        .disp-items>div {
             display: flex;
             justify-content: space-between;
             /* margin: 20px 20px 20px 20px;
             background-color: white;
             border-radius: 7px; */
         }
+
         .panel {
             margin: 30px 30px 30px 30px;
             background-color: white;
@@ -21,20 +23,43 @@
         }
     </style>
 </head>
+
 <body>
     <?php
-        define("PAGE_NAME", "home");
-        include_once("templates/header.php");
-        include_once("templates/item.php");
+    $root_path = $_SERVER["DOCUMENT_ROOT"];
+    define("PAGE_NAME", "home");
+    include_once("templates/header.php");
+    include_once("templates/item.php");
+    require_once($root_path . "/config/db.php");
     ?>
     <?php include_once("templates/slide-menu.php"); ?>
     <div class="disp-items panel">
-        <?php include_once 'templates/disp-items/disp-new-items.php' ?>
-        <?php include_once 'templates/disp-items/disp-popular-items.php' ?>
+        <div class="disp-new-items">
+        <!-- Day la doan connect voi csdl -->
+            <?php
+            $img_folder = 'public/img/items/';
+            // include 'templates/connect.php';
+            // $sql = 'select picture * price from items';
+            // $result = mysqli_query($connect, $sql);
+            $result = sql_query('select picture * price from items');
+            ?>
+            <?php foreach ($result as $each) : ?>
+                <?php spawn_item($img_folder . $each['picture'], $each['price']);
+            ?>
+            <?php endforeach ?>
+        </div>
+        <div class="disp-polular-items">
+            <?php
+            for ($i = 0; $i < 4; $i++) {
+                spawn_item("/public/img/items/shirt.jpg", "50000$");
+            }
+            ?>
+        </div>
     </div>
     <div class="disp-staff panel">
         <?php include_once("templates/counselor.php"); ?>
     </div>
     <?php include_once("templates/footer.php"); ?>
 </body>
+
 </html>
--- public/templates/item.php
+++ public/templates/item.php
@@ -5,9 +5,26 @@
         margin: 15px 15px 15px 15px;
     }
 </style>
-<?php function spawn_item($src, $price) { ?>
+<?php function spawn_item($src, $price)
+{ ?>
     <div class="item">
-        <img src="<?php echo $src; ?>" alt="Ảnh sản phẩm"><br>
-        <h5><?php echo $price; ?></h5>
+        <!-- ảnh trên và mô tả -->
+        <div>
+            <img src="<?php echo $src; ?>" alt="Ảnh sản phẩm"><br>
+            <span class="description" name="description"></span>
+        </div>
+
+        <!-- Giá và thêm vào giỏ hàng -->
+        <div class='item_cart'>
+            <div style="width: 70%; background-color:aquamarine;">
+                <h5>Price: <?php echo $price; ?></h5>
+            </div>
+
+            <a href="">
+                <div style="width: 70%; background-color:blueviolet;">
+                    Thêm vào giỏ hàng
+                </div>
+            </a>
+        </div>
     </div>
 <?php } ?>
