1e70e0f27c790c522b41ee91ab6b10d38277fd05
Dathuha <64651929+Dathuha@users.noreply.github.com>
2021-01-10 09:30:38 +0700

    Tam on phan giao dien cua item-detail

--- public/templates/item-detail.php
+++ public/templates/item-detail.php
@@ -6,6 +6,30 @@ require_once($root_path . "/public/templates/check-customer-signed-in.php");
 require_once($root_path . "/config/db.php");
 include_once($root_path . "/public/templates/item.php");
 ?>
+<?php
+
+
+// Đoạn này ông ghép cái id zô nè
+$item_id = 1;
+
+
+
+$item = sql_query("
+        SELECT *
+        FROM items
+        WHERE id = '$item_id';
+    ");
+$item = mysqli_fetch_array($item);
+
+$item_picture_src = "/public/img/items/";
+?>
+<?php
+// Get item all size types
+$item_sizes = sql_query("
+        SELECT *
+        FROM item_sizes;
+    ");
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,60 +42,69 @@ include_once($root_path . "/public/templates/item.php");
     <style>
         #page-item {
             margin-top: 80px;
-            background-color: white;
-            width: 80%;
+            margin-bottom: 40px;
             margin-left: 10%;
             margin-right: 10%;
+            background-color: white;
+            width: 80%;
             border-radius: 7px;
             box-shadow: 1px 1px 5px #ccc;
             display: flex;
+            flex-wrap: wrap;
+            flex-grow: 2;
+            padding: 20px;
+            /* height: 500px; */
         }
 
         #item-img {
-            width: 35%;
+            width: 40%;
+            height: 100%;
             padding: 20px;
             margin: auto;
-            background-color: #ffdec9;
+            /* background-color: #ffdec9; */
         }
 
         #item-img .item-image {
-            width: 100%;
-            height: 65%;
+            /* background-color: #ff8030; */
+            margin: auto;
+            /* width: 80%;
+            height: 65%; */
+            background-image: url('<?= $item_picture_src . $item['picture'] ?>');
+            width: 400px;
+            height: 400px;
+            background-size: cover;
         }
 
         #item-detail {
-            width: 65%;
-            height: 100%;
-            padding-top: 20px;
-            padding-left: 20px;
-            padding-right: 20px;
-            padding-bottom: 0px;
-            background-color: #ccc;
+            width: 60%;
+            height: 470px;
+            padding: 10px 20px 20px 20px;
+            /* background-color: #ccc; */
         }
 
         #item-detail .item-name {
             line-height: 29px;
             width: 100%;
-            background-color: #ffdec9;
+            /* background-color: #ffdec9; */
             margin-bottom: 10px;
         }
 
         #item-detail .item-price {
             line-height: 57px;
             width: 100%;
-            /* height: 100px; */
-            background-color: #ffdec9;
+            height: 200px;
+            /* background-color: #ffdec9; */
             margin-bottom: 10px;
         }
 
         #item-detail .item-size {
             width: 100%;
-            background-color: #ffdec9;
+            /* background-color: #ffdec9; */
             margin-bottom: 10px;
         }
 
         #item-detail .item-number {
-            background-color: #ffdec9;
+            /* background-color: #ffdec9; */
             width: 100%;
             /* height: 60px; */
             /* padding-top: 20px; */
@@ -79,31 +112,48 @@ include_once($root_path . "/public/templates/item.php");
         }
 
         #item-detail .div-buy-item {
-            background-color: #ffdec9;
+            /* background-color: #ffdec9; */
             width: 100%;
             /* height: 60px; */
-            /* padding-top: 20px; */
-            margin-bottom: 10px;
+            padding-bottom: 0px;
+            margin-bottom: 0px;
         }
 
         #item-detail .div-buy-item .add-to-cart {
-            width: 170px;
-            height: 40px;
-            background-color: #ffdec9;
-            border-color: #ff8030;
+            width: 200px;
+            height: 50px;
+            color: white;
+            background-color: rgba(54, 62, 126, 60%);
+            border-color: #363e7e;
             border-style: solid;
             outline: none;
+            cursor: pointer;
         }
 
         #item-detail .div-buy-item .move-to-cart {
             color: white;
-            width: 170px;
-            height: 40px;
-            background-color: #ff8030;
+            width: 200px;
+            height: 50px;
+            background-color: #363e7e;
             border-style: none;
             outline: none;
+            cursor: pointer;
         }
 
+        #div-line {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            height: 70px;
+        }
+
+        #div-description {
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
 
         /* .disp-items>div {
             display: flex;
@@ -130,52 +180,33 @@ include_once($root_path . "/public/templates/item.php");
 
     <?php include_once($root_path . "/public/templates/menu.php"); ?>
 
-    <?php
-    $item_id = 24;
-    $item = sql_query("
-            SELECT *
-            FROM items
-            WHERE id = '$item_id';
-        ");
-    $item = mysqli_fetch_array($item);
-
-    $item_picture_src = "/public/img/items/";
-    ?>
-    <?php
-        // Get item all size types
-        $item_sizes = sql_query("
-            SELECT *
-            FROM item_sizes;
-        ");
-    ?>
     <div id="page-item">
         <div id="item-img">
             <!-- ========== Ảnh sản phẩm ở đây ============= -->
             <div class="item-image">
-                <center>
-                    <img src="<?= $item_picture_src . $item['picture'] ?>" alt="Ảnh sản phẩm">
-                </center>
             </div>
         </div>
 
         <!-- ============= Thông tin về sản phẩm ================= -->
         <div id="item-detail">
             <input type="text" value="<?php echo $item["id"] ?>" hidden>
-        <!-- ============= Form để chuyển các thông tin sang giỏ hàng =========== -->
+            <!-- ============= Form để chuyển các thông tin sang giỏ hàng =========== -->
             <form action="/public/cart.php">
-                <table>
+                <table style="height: 100%; width: 100%; color: #363e7e;">
                     <tr class="item-name">
                         <td colspan="3">
-                            <h3>
+                            <span style="font-size: 30px;">
                                 <?= $item["name"] ?>
-                            </h3>
+                            </span>
                         </td>
                     </tr>
                     <tr class="item-price">
                         <td colspan="3">
-                            <h1>
-                                <?= $item["price"] ?>
-                            </h1>
+                            <b>
+                                <span style="font-size: 45px; vertical-align: top;">
+                                    <?= $item["price"] ?> $
+                                </span>
+                            </b>
                         </td>
                     </tr>
                     <tr class="item-size">
@@ -183,9 +214,9 @@ include_once($root_path . "/public/templates/item.php");
                             <span>Size</span>
                         </td>
                         <td colspan="2">
-                            <?php foreach ($item_sizes as $each): ?>
+                            <?php foreach ($item_sizes as $each) : ?>
                                 <label id="<?php echo $each['size']; ?>">
-                                    <input type="radio" name="size" id="<?php echo $each['size']; ?>"><?php echo $each['size']; ?>
+                                    <input type="radio" name="size" value="<?php echo $each['size']; ?>" id="<?php echo $each['size']; ?>"><?php echo $each['size']; ?>
                                 </label>
                             <?php endforeach ?>
                         </td>
@@ -199,13 +230,13 @@ include_once($root_path . "/public/templates/item.php");
                             <input type="number" value='1' name="number" style="width: 50px;">
                             <input type="button" value="+">
                         </td>
-                        <td>
+                        <!-- <td>
                             <span>
                                 Còn lại
-                                    <input type="text" value="12" disabled>
+                                <input type="text" value="12" disabled>
                                 sản phẩm
                             </span>
-                        </td>
+                        </td> -->
                     </tr>
                     <tr class="div-buy-item">
                         <td colspan="3">
@@ -218,9 +249,21 @@ include_once($root_path . "/public/templates/item.php");
                 </table>
             </form>
         </div>
+        <div id="div-line">
+            <div style="width: 90%; border-top: 2px #e0e0e0 solid;">
+            </div>
+        </div>
+        <div id="div-description">
+            <div style="width: 90%;">
+                <h1>Mô tả sản phẩm</h1>
+                <br>
+                <?php echo $item['description']; ?>
+            </div>
+        </div>
     </div>
 
-    <?php include_once($root_path . "/public/templates/counselor.php"); ?>
+    <!-- <?php //include_once($root_path . "/public/templates/counselor.php"); 
+            ?> -->
 
     <!--///////////////  Here is include footer /////////////-->
     <?php include_once($root_path . "/public/templates/footer.php"); ?>
