72df011b817d9e46894a4c8d3b3f6649565440aa
Dathuha <64651929+Dathuha@users.noreply.github.com>
2021-01-16 14:43:29 +0700

    Done qna qna in customer

--- public/qna.php
+++ public/qna.php
@@ -5,6 +5,11 @@ define("PAGE_NAME", "qna");
 require_once($root_path . "/public/templates/check-customer-signed-in.php");
 require_once($root_path . "/config/db.php");
 include_once($root_path . "/public/templates/item.php");
+
+$qnas = sql_query("
+        select *
+        from qna;
+    ");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,21 +22,39 @@ include_once($root_path . "/public/templates/item.php");
     <link rel="icon" href="/public/img/socials/logo_1.png">
     <style>
         .panel {
+            font-size: 20px;
             margin: 30px 10% 30px 10%;
             background-color: white;
+            color: white;
             border-radius: 7px;
             box-shadow: 1px 1px 5px #ccc;
             min-height: 300px;
+            min-width: 1000px;
+            display: flex;
+            justify-content: center;
         }
 
-        .notification {
-            width: 100%;
-            padding: 10%;
-            text-align: center;
+        .content {
+            margin-top: 5%;
+            margin-bottom: 5%;
+            width: 80%;
+            height: 100%;
         }
-        .notification * {
-            margin-bottom: 15px;
+        .content .question {
+            background-color: #599ef7;
+            padding: 17px;
+            margin-top: 20px;
+            border-radius: 5px;
+            cursor: pointer;
         }
+        .content .answer {
+            color: black;
+            background-color: #e3e3e3;
+            padding: 17px;
+            margin-bottom: 20px;
+            border-radius: 5px;
+        }
+
     </style>
 </head>
 <body>  
@@ -39,12 +62,31 @@ include_once($root_path . "/public/templates/item.php");
     <?php require_once($root_path . '/public/templates/menu.php'); ?>
     
     <div class="panel">
-        <div class="notification">
-            <h2>Chúng tôi đang trong quá trình biên soạn câu hỏi</h2>
-            <h3>Mời bạn vui lòng quay lại sau!</h3>
+        <div style="color: black; padding-top: 20px;">
+            <h1>Hỏi đáp</h1>
+        </div>
+        <div class="content">
+            <?php foreach ($qnas as $qna) : ?>
+                <div id="question<?php echo $qna['id'] ?>" class="question" onclick="show_answer(<?php echo $qna['id'] ?>)">
+                    <?= $qna['question'] ?>
+                </div>
+                <div id="answer<?php echo $qna['id'] ?>" class="answer" hidden>
+                    <?= $qna['answer'] ?>
+                </div>
+            <?php endforeach ?>
         </div>
     </div>
 
     <?php require_once($root_path . '/public/templates/footer.php'); ?>
 </body>
+<script>
+    function show_answer(qna_id) {
+        var show_hide_answer = document.getElementById("answer" + qna_id);
+        if (show_hide_answer.style.display === "block") {
+            show_hide_answer.style.display = "none";
+        } else {
+            show_hide_answer.style.display = "block";
+        }
+    }
+</script>
 </html>
