88606491506ed7326e415abd8b2ce8a46922febe
Dathuha <congtrieu.ng06@gmail.com>
2021-01-28 01:24:48 +0700

    done contact in homepage

---
+++ manager/contact/view-contact.php
@@ -0,0 +1,86 @@
+<?php
+    define("MENU_OPTION", "contact");
+    $root_path = $_SERVER["DOCUMENT_ROOT"];
+    
+    // Check signed in
+    require_once($root_path . "/manager/templates/check-admin-signed-in.php");
+    check_admin_signed_in(2);
+
+    require_once($root_path . "/config/db.php");
+    require_once($root_path . "/config/background.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="HandheldFriendly" content="true">
+    
+    <title>Quản lý background</title>
+    <link rel="stylesheet" href="/manager/templates/css/all.css">
+    <link rel="stylesheet" href="/manager/templates/css/layout.css">
+    <style>
+        #display-size {
+            width: 100%;
+            border: 1px #ccc solid;
+            border-collapse: collapse;
+            /* margin: 5px 5px 5px 5px; */
+        }
+        #display-size tr td {
+            border: 1px #ccc solid;
+            padding: 5px 5px 5px 5px;
+        }
+    </style>
+</head>
+<body>
+    <!-- Header menu -->
+    <?php include_once($root_path . "/manager/templates/header.php"); ?>
+    <div class="page-body">
+        <!-- Slide menu -->
+        <div class="page-menu">
+            <?php include_once($root_path . "/manager/templates/menu.php"); ?>
+        </div>
+        <!-- Main content -->
+        <?php
+            $sql_phone = "
+                select *
+                from contact
+                where id = 1
+            ";
+            $sql_address = "
+                select *
+                from contact
+                where id = 2
+            ";
+            $phone = mysqli_fetch_array(sql_query($sql_phone));
+            $address = mysqli_fetch_array(sql_query($sql_address));
+        ?>
+        <div class="page-content">
+            <div class="scrollable">
+                <table id="content-table">
+                    <tr class="table-bar-header" style="top: 0;">
+                        <td>Số điện thoại liên hệ</td>
+                        <td>Địa chỉ</td>
+                        <td>Sửa thông tin</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <?= $phone["value"] ?>
+                        </td>
+                        <td>
+                            <?= $address["value"] ?>
+                        </td>
+                        <td>
+                            <a href="">
+                                Sửa
+                            </a>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- public/templates/ui/footer.php
+++ public/templates/ui/footer.php
@@ -1,3 +1,32 @@
+<?php
+    $root_path = $_SERVER["DOCUMENT_ROOT"];
+    require_once($root_path . "/public/templates/account/check-customer-signed-in.php");
+    require_once($root_path . "/manager/templates/notification-page.php");
+    if (basename($_SERVER['PHP_SELF']) == "header.php") {
+        display_notification_page(
+            false,
+            "Quản lý admin",
+            "404",
+            "Không tìm thấy trang",
+            "Quay lại"
+            // Quay về trang trước
+        );
+        exit();
+    }
+
+    $sql_phone = "
+        select *
+        from contact
+        where id = 1
+    ";
+    $sql_address = "
+        select *
+        from contact
+        where id = 2
+    ";
+    $phone = mysqli_fetch_array(sql_query($sql_phone));
+    $address = mysqli_fetch_array(sql_query($sql_address));
+?>
 <style>
     #div_all_footer {
         background-color: #363e7e;
@@ -21,10 +50,6 @@
     .title {
         font-family: Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     }
-
-    /*#div_content_inside .sign-up {
-        width: 33%;
-    }*/
     #div_content_inside .contact {
         width: 34%;
     }
@@ -38,39 +63,14 @@
 
 <div id="div_all_footer">
     <div id="div_content_inside">
-        <!-- <div class="sign-up">
-            <h2 class="title">Đăng kí</h2><br>
-            <div>
-                <?php //include_once("sign-up.php"); ?>
-            </div>
-        </div> -->
-
-        <div class="contact">
+        <div>
             <h2 class="title">Liên hệ</h2><br>
-            <!-- <div>
-                <a href="#" class="icon">
-                    <img width="50" height="50" src="/public/img/socials/facebook-icon.png" alt="Facebook">
-                </a>
-                <a href="#" class="icon">
-                    <img width="50" height="50" src="/public/img/socials/google-icon.png" alt="Gmail">
-                </a>
-                <a href="#" class="icon">
-                    <img width="50" height="50" src="/public/img/socials/zalo-icon.png" alt="Zalo">
-                </a>
-            </div> -->
-            <span style="font-weight: bold;">Địa chỉ</span>: <br>Tầng 28, Tòa nhà trung tâm Lotte Hà Nội, 54 Liễu Giai, phường Cống Vị, Quận Ba Đình, Hà Nội.
-            <br><br>
-            <span style="font-weight: bold;">Tổng đài hỗ trợ</span>: <br>19001221
+            <span style="font-weight: bold;">Tổng đài hỗ trợ</span>: <br>
+            <?= $phone["value"] ?>
+            <br>
+            <br>
+            <span style="font-weight: bold;">Địa chỉ</span>: <br>
+            <?= $address["value"] ?>
         </div>
-
-        <!-- <div class="map">
-            <h2 class="title">Địa chỉ</h2><br>
-            <div>
-                <h4 class="title">
-                    A17 Lê Thanh Nghị, Hai Bà Trưng, Hà Nội
-                </h4>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.7617995886294!2d105.84454931533641!3d21.00218339406003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe0dd0b2919d48ce9!2zSOG7jWMgdmnhu4duIENOVFQgQsOhY2ggS2hvYQ!5e0!3m2!1svi!2s!4v1609259670369!5m2!1svi!2s" width="440" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-            </div>
-        </div> -->
     </div>
 </div>
